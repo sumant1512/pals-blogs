9381433f4730ffdaf2540a25dd9651c3da14e9b8
sumant1512 <sumantmishra511@gmail.com>
2025-01-14 01:41:12 +0530

    Blogs models added

---
+++ backend/models/Blogs.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const BlogsSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  subTitle: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+});
+
+const Blogs = mongoose.model("blogs", BlogsSchema);
+Blogs.createIndexes();
+module.exports = Blogs;
